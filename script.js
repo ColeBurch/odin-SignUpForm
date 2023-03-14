7f5034f9508fd5798867acf75dc3b45f68f7a529
ColeBurch <48129706+ColeBurch@users.noreply.github.com>
2023-03-13 20:08:55 -0600

    Clean script.js by removing console.logs and duplicate code

--- script.js
+++ script.js
@@ -3,12 +3,8 @@ let confirmPassword = document.querySelector("#confirm_password")
 const confirmPasswordBox = document.querySelector(".confirmBox")
 const passwordsDoNotMatch = document.createElement("div")
 passwordsDoNotMatch.textContent = "Passwords do not match"
-console.log(passwordsDoNotMatch.textContent)
 
-console.log(password)
-console.log(confirmPassword)
-
-password.oninput = function () {
+function checkPasswords() {
     let password = document.querySelector("#password")
     let confirmPassword = document.querySelector("#confirm_password")
     if (password.value != confirmPassword.value) {
@@ -22,16 +18,5 @@ password.oninput = function () {
     }
 }
 
-confirmPassword.oninput = function () {
-    let password = document.querySelector("#password")
-    let confirmPassword = document.querySelector("#confirm_password")
-    if (password.value != confirmPassword.value) {
-        confirmPasswordBox.appendChild(passwordsDoNotMatch)
-        password.setCustomValidity("Invalid field.")
-        confirmPassword.setCustomValidity("Invalid field.")
-    } else {
-        confirmPasswordBox.removeChild(passwordsDoNotMatch)
-        password.setCustomValidity("")
-        confirmPassword.setCustomValidity("")
-    }
-}
+password.oninput = checkPasswords
+confirmPassword.oninput = checkPasswords
